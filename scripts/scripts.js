0103741f21be3f8f32dea4fbdaf4012264044c2e
Carol Marques <cmarques.carolina03@gmail.com>
2023-07-28 18:37:01 -0300

    starting study about js

---
+++ scripts/scripts.js
@@ -0,0 +1,25 @@
+// // document.getElementById('nome').value = 'Oi'
+
+// // string
+// var texto = 'Curso de JavaScript'
+
+// // number
+// var numeroInteiro = -7
+// var numeroFracionado = 10.75
+
+// // boolean
+// var verdadeiro = true;
+// var falso = false;
+
+// // console.log('numero fracionado', numeroFracionado)
+// // document.write(texto)
+
+var media = 7;
+
+var nota1 = prompt('Dê a nota:')
+
+if (nota1 <= media) {
+    document.write('Aprovado')
+} else {
+    document.write('Reprovado')
+}
